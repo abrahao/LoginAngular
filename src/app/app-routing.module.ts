89a323fdc29b8f3f8b215100713c8876fb9e81b6
abrahao <abrahao.eneias@dce.ufpb.br>
2021-05-02 16:08:46 -0300

    Add rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from "./create-account/create-account.component";
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component: LoginComponent },
+  { path: 'create-account', component: CreateAccountComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
